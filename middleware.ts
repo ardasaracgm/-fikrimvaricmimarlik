510704ef128aa594b08a080cf2921d12381af105
ardasaracgm <ardasaracgm@gmail.com>
2026-03-28 21:50:09 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isStatic =
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.includes(".");
+
+  if (isStatic) {
+    return NextResponse.next();
+  }
+
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/tr", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"]
+};
