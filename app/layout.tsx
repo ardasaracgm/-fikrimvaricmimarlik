89f4044b5d494b80d3dd249aaad08c5efb59dbe4
ardasaracgm <ardasaracgm@gmail.com>
2026-03-28 21:45:31 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Hauzart",
+  description: "Interior design, implementation and fitout management."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
